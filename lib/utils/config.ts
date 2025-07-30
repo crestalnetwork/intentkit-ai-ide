60fa43b037c7e76df87c9ca29f5dd9710f6b8c23
spikedingo <spikedingo@gmail.com>
2025-07-30 16:00:03 +0800

    fix: build error

--- lib/utils/config.ts
+++ lib/utils/config.ts
@@ -1,6 +1,6 @@
 // API Configuration
 export const LOCAL_BASE_URL = "http://127.0.0.1:8000";
-export const DEFAULT_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const DEFAULT_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? '';
 
 // Default configuration
 export const DEFAULT_CONFIG = {
